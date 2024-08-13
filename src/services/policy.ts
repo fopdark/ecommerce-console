363403015b4f432e8b29bcdd320c0ae702113bfb
Henry Nguyen <fopdark@gmail.com>
2024-08-13 13:32:49 +0700

    add api save policies

--- src/services/policy.ts
+++ src/services/policy.ts
@@ -2,25 +2,25 @@ import axios from "axios";
 import { API_URL } from "@/constant/ConstantCommon";
 
 export function getList(requestParams: any) {
-  return axios.get(`${API_URL}/admin/policy`, requestParams).then((res) => {
+  return axios.get(`${API_URL}/admin/policies`, requestParams).then((res) => {
     return res.data.data;
   });
 }
 
 export function create(requestParams: any) {
-  return axios.post(`${API_URL}/admin/policy`, requestParams).then((res) => {
+  return axios.post(`${API_URL}/admin/policies`, requestParams).then((res) => {
     return res.data.data;
   });
 }
 
 export function update(id: string, requestParams: any) {
-  return axios.put(`${API_URL}/admin/policy/${id}`, requestParams).then((res) => {
+  return axios.put(`${API_URL}/admin/policies/${id}`, requestParams).then((res) => {
     return res.data.data;
   });
 }
 
 export function deletePolicy(id: string) {
-  return axios.delete(`${API_URL}/admin/policy/${id}`).then((res) => {
+  return axios.delete(`${API_URL}/admin/policies/${id}`).then((res) => {
     return res.data.data;
   });
 }
